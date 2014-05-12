8e7fff455559a7f9d16b301e42a4ce2cd8efe143
Steve Sloan <steve@finagle.org>
2014-05-11 23:22:58 -0700

    Support for Moped v2/Mongoid v4.

--- lib/em-synchrony/moped.rb
+++ lib/em-synchrony/moped.rb
@@ -7,33 +7,31 @@ end
 # monkey-patch Moped to use em-synchrony's socket
 silence_warnings do
   module Moped
-    module EMSockets
-      class TCP < EventMachine::Synchrony::TCPSocket
-        include Moped::Sockets::Connectable
+    class EM_TCP < EventMachine::Synchrony::TCPSocket
+      include defined?(Moped::Sockets) ? Moped::Sockets::Connectable : Moped::Connection::Socket::Connectable
 
-        def self.connect(*args)
-          host, port, timeout = args
-          begin
-            super
-          rescue SocketError => ex
-            raise Errors::ConnectionFailure, "#{host}:#{port}: #{ex.class.name}: #{ex.message}"
-          end
+      def self.connect(*args)
+        host, port, timeout = args
+        begin
+          super
+        rescue SocketError => ex
+          raise Errors::ConnectionFailure, "#{host}:#{port}: #{ex.class.name}: #{ex.message}"
         end
+      end
 
-        def set_encoding(enc)
-          raise "Can't set socket encoding to #{enc.inspect}"  unless %w(binary ascii-8bit).include?(enc.downcase)
-        end
+      def set_encoding(enc)
+        raise "Can't set socket encoding to #{enc.inspect}"  unless %w(binary ascii-8bit).include?(enc.downcase)
+      end
 
-        def alive?
-          !error?
-        end
+      def alive?
+        !error?
       end
     end
 
     class Connection
       def connect
         raise "SSL not supported"  if !!options[:ssl]
-        @sock = Moped::EMSockets::TCP.connect(host, port, timeout)
+        @sock = Moped::EM_TCP.connect(host, port, timeout)
       end
     end
   end
